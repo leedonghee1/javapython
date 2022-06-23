b44db40469dcecffa003d94e9fa5775ce511aacb
leedonghee1 <ldh960721@naver.com>
2022-06-23 17:04:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# javapython
+# javapython
---
+++ java_work/ex44/src/ex44/AA.java
@@ -0,0 +1,16 @@
+package ex44;
+
+	public static void main(String[] args)
+	
+	// ctrl + y 앞으로 가기
+	// ctrl + shift + f 자동정렬
+    // ctrl + f11
+
+		System.out.println("안녕하세요");
+		System.out.println("010-9946-2662");
+		System.out.println("z");
+		System.out.println("zz");
+		System.out.println(3+3);
+		
+	}
+}
